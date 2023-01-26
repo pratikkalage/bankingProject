58d5ff83af9d0894d8ba6383ad3018cefe0a2cc4
Pratik Kalage <pratikkalage4555@gmail.com>
2023-01-26 15:40:34 +0530

    DGTL-101:Done Baseclass Changes

---
+++ src/main/java/com/finance/base/BaseClass.java
@@ -0,0 +1,18 @@
+package com.finance.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		System.setProperty("webdrivr.chrome.driver", "E:\\Softwares\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://demo.guru99.com/V1/index.php");
+		
+		
+		
+		
+	}
+
+}
