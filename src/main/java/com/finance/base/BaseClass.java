a57f6006bcaa7e415f5d14e26d0a8efbe406e0f2
Pratik Kalage <pratikkalage4555@gmail.com>
2023-01-31 20:00:48 +0530

    DGTL-106:Done changes in Base Class....

--- src/main/java/com/finance/base/BaseClass.java
+++ src/main/java/com/finance/base/BaseClass.java
@@ -8,16 +8,7 @@ public class BaseClass {
 	public static void main(String[] args) {
 		System.setProperty("webdrivr.chrome.driver", "E:\\Softwares\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
-		driver.get("https://demo.guru99.com/V1/index.php");
-
-		
-		
-		
-		
-		
-		
-		
-		
+		driver.get("https://demo.guru99.com/V1/index.php");		
 	}
 
 }
